b54c566ae282de638915b5ef9b36318b252611a4
taras1808 <taras1808@gmail.com>
2020-11-12 16:12:45 +0100

    SHOP.SERVER-2 Query for category products

--- src/controllers/CategoriesController.js
+++ src/controllers/CategoriesController.js
@@ -36,28 +36,70 @@ exports.getPrices = (req, res) => {
 
 exports.getProducts = (req, res) => {
 
-    if (req.query.price && req.query.price != '') {
-        if (req.query.producers && req.query.producers != '') {
+    let orderBy
+    let order
+
+    switch (req.query.orderBy) {
+        case '0':
+            orderBy = 'id'
+            order = 'DESC'
+            break
+        case '1':
+            orderBy = 'id'
+            order = 'ASC'
+            break
+        case '2':
+            orderBy = 'price'
+            order = 'ASC'
+            break
+        case '3':
+            orderBy = 'price'
+            order = 'DESC'
+            break
+        case '4':
+            orderBy = 'name'
+            order = 'ASC'
+            break
+        case '5':
+            orderBy = 'name'
+            order = 'DESC'
+            break
+        default:
+            orderBy = 'id'
+            order = 'ASC'
+            break
+    }
+
+    if (req.query.price) {
+        if (req.query.producers) {
             Category.relatedQuery('products')
                 .for(req.params.categoryId)
                 .whereIn('producer_id', req.query.producers.split(','))
-                .andWhereBetween('price', req.query.price.split(','))
+                .andWhereBetween('price', req.query.price.split('-'))
+                .orderBy(orderBy, order)
                 .then(result => res.json(result))
         } else {
             Category.relatedQuery('products')
                 .for(req.params.categoryId)
-                .andWhereBetween('price', req.query.price.split(','))
+                .andWhereBetween('price', req.query.price.split('-'))
+                .orderBy(orderBy, order)
                 .then(result => res.json(result))
         }
     } else {
-        if (req.query.producers && req.query.producers != '') {
+        if (req.query.producers) {
             Category.relatedQuery('products')
                 .for(req.params.categoryId)
                 .whereIn('producer_id', req.query.producers.split(','))
+                .orderBy(orderBy, order)
                 .then(result => res.json(result))
         } else {
             Category.relatedQuery('products')
                 .for(req.params.categoryId)
+                .orderBy(orderBy, order)
+                .then(result => {
+                        // console.log(result)
+                return result
+                })
                 .then(result => res.json(result))
         }
     }
