2f940d105807691d53b5e48362038de567f0b3f3
taras1808 <taras1808@gmail.com>
2020-11-21 15:01:58 +0100

    SHOP.SERVER-8 Product create, edit, delete

--- src/controllers/ProductsController.js
+++ src/controllers/ProductsController.js
@@ -1,5 +1,8 @@
 const Product = require('../models/Product')
 const Category = require('../models/Category')
+const Knex = require('../models/Knex');
+const { raw } = require('objection');
+
 
 exports.getProducts = (req, res) => {
 
@@ -16,37 +19,52 @@ exports.getOptions = (req, res) => {
 
 }
 
-exports.create = (req, res) => {
+exports.create = async (req, res) => {
 
-    if (!req.files || Object.keys(req.files).length === 0) {
-        return res.status(400).end();
+    if (!req.body) {
+        return res.status(400).end()
     }
 
-    var file = req.files.file
+    if (!req.body.name || req.body.name.length === 0) {
+        return res.status(400).end()
+    }
 
-    if (!file) return res.status(400).end();
+    const exists = await Product.query()
+        .where(raw('lower("name")'), req.body.name.toLowerCase())
 
-    file.mv('public/' + file.name, async function(err) {
-        if (err) return res.status(500).json(err)
-        var path = 'http://' + req.hostname + ':' + req.socket.localPort + '/' + file.name
+    if (exists.length > 0) {
+        return res.status(409).end()
+    }
 
-        const product = await Product.query()
-            .insertAndFetch({
-                name: req.body.name,
-                price: req.body.price,
-                old_price: req.body.old_price.length === 0 ? null : req.body.old_price,
-                image: path,
-                category_id: req.body.category
-            })
+    const product = await Product.query()
+        .insertAndFetch({
+            name: req.body.name,
+            price: req.body.price,
+            old_price: req.body.old_price,
+            category_id: req.body.category_id
+        })
 
-        JSON.parse(req.body.options).forEach(async e => {
-            const [id, value] = e
-            const knex = Product.knex();
-            await knex.raw(`INSERT INTO filter_product_option VALUES(${id}, ${product.id}, ${value})`);
-        });
+    JSON.parse(req.body.options).forEach(async e => {
+        const [id, value] = e
+        await Knex.raw(`INSERT INTO filter_product_option VALUES(${id}, ${product.id}, ${value})`);
+    });
 
+    const file = req.files ? req.files.image : null
+
+    if (file) {
+        file.mv('public/' + file.name, function(err) {
+            if (err) return res.status(500).json(err)
+            var path = 'http://' + req.hostname + ':' + req.socket.localPort + '/' + file.name
+            
+            Product.query()
+                .patchAndFetchById(product.id, {
+                    image: path
+                })
+                .then(result => res.json(result))
+        })
+    } else {
         res.json(product)
-    })
+    }
 }
 
 exports.getProduct = async (req, res) => {
@@ -58,32 +76,61 @@ exports.getProduct = async (req, res) => {
     res.json(product)
 }
 
-exports.update =  async (req, res) => {
+exports.update = async (req, res) => {
+
+    if (!req.body) {
+        return res.status(400).end()
+    }
+
+    if (!req.body.name || req.body.name.length === 0) {
+        return res.status(400).end()
+    }
+
+    const exists = await Product.query()
+        .where(raw('lower("name")'), req.body.name.toLowerCase())
+        .whereNot('id', req.params.productId)
+
+    if (exists.length > 0) {
+        return res.status(409).end()
+    }
 
     const product = await Product.query()
         .patchAndFetchById(req.params.productId, {
             name: req.body.name,
             price: req.body.price,
             old_price: req.body.old_price,
-            // image: path,
+            image: req.body.image ?? null,
             category_id: req.body.category_id
         })
 
     await Product.relatedQuery('options').for(product).unrelate()
 
-    req.body.options.forEach(async e => {
+    JSON.parse(req.body.options).forEach(async e => {
         const [id, value] = e
         const knex = Product.knex();
         await knex.raw(`INSERT INTO filter_product_option VALUES(${id}, ${product.id}, ${value})`);
     });
 
-    res.json(product)
+    const file = req.files ? req.files.file : null
+
+    if (file) {
+        file.mv('public/' + file.name, function(err) {
+            if (err) return res.status(500).json(err)
+            var path = 'http://' + req.hostname + ':' + req.socket.localPort + '/' + file.name
+            
+            Product.query()
+                .patchAndFetchById(product.id, {
+                    image: path
+                })
+                .then(result => res.json(result))
+        })
+    } else {
+        res.json(product)
+    }
 }
 
 exports.delete =  (req, res) => {
-
     Product.query()
         .deleteById(req.params.productId)
         .then(result => res.json(result))
-
 }
